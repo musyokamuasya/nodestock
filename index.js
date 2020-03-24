bc35742dc386e100837b445efc0b83a997c9a42e
Musyoka Muasya <josephmuasya8@gmail.com>
2020-03-25 01:38:58 +0300

    Connected about and home to handlebars

--- index.js
+++ index.js
@@ -10,13 +10,24 @@ const PORT = process.env.PORT||5000;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+// IEK APIs
+// API Token: pk_2d5e072944934e17893ee7396ea3dbd8 
+// Account No. edf918ea98e365a78c08fd91c7ccf32f 
  
+
+//Main page route
 app.get('/', function (req, res) {
     res.render('home', {
+
+    });
+});
+// About Page route
+app.get('/about.html', function (req, res) {
+    res.render('about', {
         
     });
 });
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
